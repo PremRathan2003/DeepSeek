e5f4e66092cf52cc21d2c9b6006c3212ff61fcb6
PremRathan2003 <premrathanaddanki7736@gmail.com>
2025-05-18 23:20:45 +0100

    Updated middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import { withClerkMiddleware } from '@clerk/nextjs';
-import { WithClerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default withClerkMiddleware();
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
